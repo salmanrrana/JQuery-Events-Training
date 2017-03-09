4d24d35afb0ab783751dfbc2cb43cf073fe6df23
Salman Rana <salman.r.rana@gmail.com>
2017-03-09 08:32:10 -0600

    thank you for your wisdom Rene

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,27 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+console.log("Sanity Check: JS is working!");
 
+$(document).ready(function(){
   $("#time").text( Date.now() );
 
+  $('input').keyup(function(){
+    var a = parseInt($('#left').val());
+    var b =   parseInt($('#right').val());
+    var total = a + b;
+$('#total').val(total);
+  });
+
+
+
+
+
 })
+
+
+// // pseudocode:
+// // left + right = (total)put the total of left and rightover here
+// // this is the left number #left
+// // this is the right number #right
+// // add both of those numbers #left + #right
+// // put the value of those numbers in total  total
