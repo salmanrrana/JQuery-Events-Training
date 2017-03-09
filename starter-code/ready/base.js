669413cba6d6d8fbd9818d423cac8784116a4735
Salman Rana <salman.r.rana@gmail.com>
2017-03-08 19:27:03 -0600

    I think I understand what I did

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -3,8 +3,8 @@
 */
 
 $(document).ready(function(){
-  console.log( "The page says:", $("h1").text() );
-});
+  // console.log( "The page says:", $("h1").text() );
+
 
 $("h1").text("Go!");
 
@@ -14,3 +14,5 @@ if ( $("h1").text() === "Go!" ){
 } else {
   console.log( 'Can you make it say "Go!"?' );
 }
+// I moved the curly bracket and bracket to the bottom of the function in order for the DOM to be ready to change?
+});
